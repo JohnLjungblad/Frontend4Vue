dc39e53bef70c2658747de2d4c39f9bdd5994f23
JohnLjungblad <ljungblad.john@gmail.com>
2024-03-17 10:55:48 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-})
+const { defineConfig } = require('@vue/cli-service');
 
-module.exports = {
+module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
     ? '/my-project/'
-    : '/'
-}
+    : '/',
+  transpileDependencies: true,
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].template = './public/index.html';
+      return args;
+    });
+  }
+});
